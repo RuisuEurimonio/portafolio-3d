2bbbe8388fb26ebbab3bfdeafb3060bf00ceb96f
Ruisu Eurimonio <pipelinares2912@gmail.com>
2025-04-23 02:39:04 -0500

    Se ajustan sombras

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,28 @@ import { Canvas } from '@react-three/fiber'
 import './App.css'
 import { OrbitControls } from '@react-three/drei'
 import MainScene from './MainScene'
+import { EffectComposer, Noise } from '@react-three/postprocessing'
 
 function App() {
 
   return (
     <Canvas camera={{position: [-10,10,20], fov: 50}} style={{width: "100vw", height: "100vh"}}
     onCreated={({ gl }) => {
-      gl.setClearColor('#0c0010 ')
+      gl.setClearColor('#000000')
     }}
+    shadows
     >
-      <ambientLight intensity={1}/>
-      <directionalLight position={[0, 1, 0]} intensity={3} />
+      <EffectComposer>
+        <Noise opacity={0.05} />
+      </EffectComposer>
+      <ambientLight intensity={0.05} />
+      <spotLight 
+  position={[0, 10, 0]}
+  angle={0.8}
+  penumbra={0.5}
+  intensity={20}
+  castShadow
+/>
       <OrbitControls />
       <MainScene />
     </Canvas>
